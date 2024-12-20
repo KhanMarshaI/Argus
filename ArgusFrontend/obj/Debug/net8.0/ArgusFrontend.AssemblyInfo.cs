6784a582273de2842578b84c46dd49cbf2fe7074
KhanMarshaI <iamtaha05@gmail.com>
2024-12-20 23:56:30 +0500

    Modify CSS

--- ArgusFrontend/obj/Debug/net8.0/ArgusFrontend.AssemblyInfo.cs
+++ ArgusFrontend/obj/Debug/net8.0/ArgusFrontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArgusFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e60ce59133e2acacc5a621a5d237814e0767cdff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac5c60d2f60f3fc372d6b2c7adb574f7d62bcd14")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArgusFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArgusFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
